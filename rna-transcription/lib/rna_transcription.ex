b633114068311901260dd76e6a6d97525db895d3
Matt Levy <milevy1@gmail.com>
2019-08-05 10:00:40 -0600

    Adds case statement to convert individual chars

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -6,8 +6,26 @@ defmodule RnaTranscription do
 
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
+
+  Given a DNA strand, its transcribed RNA strand is formed by
+  replacing each nucleotide with its complement:
+  G -> C
+  C -> G
+  T -> A
+  A -> U
+
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    # Build case switch for complments
+    case dna do
+      'G' -> 'C'
+      'C' -> 'G'
+      'T' -> 'A'
+      'A' -> 'U'
+      _ -> '?'
+    end
+
+    # Iterate through each char in string with complements case
   end
 end
