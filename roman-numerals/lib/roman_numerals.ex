4cdbe5ccb187f176b18409d6f1093ce5614954de
Matt Levy <milevy1@gmail.com>
2019-08-07 14:30:52 -0600

    Adds roman_numerals solution using Enmu .find, .find_index, .at

--- roman-numerals/lib/roman_numerals.ex
+++ roman-numerals/lib/roman_numerals.ex
@@ -1,8 +1,14 @@
 defmodule RomanNumerals do
-  @doc """
-  Convert the number to a roman number.
-  """
-  @spec numeral(pos_integer) :: String.t()
+  @integers [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  @romans ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+  def numeral(0), do: ""
+
   def numeral(number) do
+    integer = Enum.find(@integers, fn integer -> number >= integer end)
+    integer_index = Enum.find_index(@integers, fn integer -> number >= integer end)
+    roman_numeral = Enum.at(@romans, integer_index)
+
+    roman_numeral <> numeral(number - integer)
   end
 end
