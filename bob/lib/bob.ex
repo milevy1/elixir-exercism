f4ff54e72a2381e84220b301924df1c0f73ac8a8
Matt Levy <milevy1@gmail.com>
2019-08-08 16:29:18 -0600

    Adds secondary guard to is_shouting?

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -2,10 +2,21 @@ defmodule Bob do
 
    def hey(string) do
      cond do
+       is_silent?(string) -> "Fine. Be that way!"
+       is_shouting_question?(string) -> "Calm down, I know what I'm doing!"
        is_question?(string) -> "Sure."
        is_shouting?(string) -> "Whoa, chill out!"
        true -> "Whatever."
      end
+   end
+
+   def is_silent?(""), do: true
+   def is_silent?(string) do
+     String.trim(string) == ""
+   end
+
+   def is_shouting_question?(string) do
+     is_question?(string) && is_shouting?(string)
    end
 
    def is_question?(string) do
@@ -13,7 +24,8 @@ defmodule Bob do
    end
 
    def is_shouting?(string) do
-     string == String.upcase(string)
+     string == String.upcase(string) &&
+      string != String.downcase(string)
    end
 
 end
