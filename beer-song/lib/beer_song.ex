59eb4c47f9fac4c59b37712e2bb30a6e099e5a8f
Matt Levy <milevy1@gmail.com>
2019-08-11 10:09:33 -0600

    Adds recursive solution to beer_song

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -3,15 +3,41 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    # Your implementation here...
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    # Your implementation here...
+  def lyrics, do: lyrics(99..0)
+  def lyrics(0..0), do: verse(0)
+  def lyrics(first..last) do
+    verse(first) <> "\n" <> lyrics(first - 1..last)
   end
 end
