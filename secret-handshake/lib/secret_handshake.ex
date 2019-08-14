6d16ba567aa0af58bc4c6f2ada40a63cb0476abb
Matt Levy <milevy1@gmail.com>
2019-08-14 11:24:46 -0600

    Adds secret-handshake solution

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -14,6 +14,31 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
+  def commands(code) when code <= 0 or code >= 32, do: []
   def commands(code) do
+    Integer.to_string(code, 2)
+    |> String.graphemes
+    |> Enum.reverse
+    |> Stream.with_index
+    |> reverse?
+    |> Enum.map(fn x -> secret(x) end)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  def secret(x) do
+    case x do
+      {"1", 0} -> "wink"
+      {"1", 1} -> "double blink"
+      {"1", 2} -> "close your eyes"
+      {"1", 3} -> "jump"
+      {_, _} -> nil
+    end
+  end
+
+  def reverse?(list) do
+    cond do
+      Enum.at(list, 4) == {"1", 4} -> Enum.reverse(list) |> Enum.slice(1..-1)
+      true -> list
+    end
   end
 end
