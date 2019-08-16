4ae859804c06f5cb583b5dd513900cbb2d547532
Matt Levy <milevy1@gmail.com>
2019-08-16 11:55:16 -0600

    Adds partial solution ot rotational cipher

--- rotational-cipher/lib/rotational_cipher.ex
+++ rotational-cipher/lib/rotational_cipher.ex
@@ -8,5 +8,19 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> String.to_charlist
+    |> Enum.map(fn char ->
+      cond do
+        char in [48, 49, 50, 51, 52, 53, 54, 55, 56, 57] -> char # integers
+        char == 32 -> 32
+        char + shift > 122 ->
+          char - rem(shift, 26) # 122 == 'z' so looping alphabet
+        true -> char + shift
+      end
+    end)
+    |> List.to_string
   end
+
+  defp upcase?(char), do: char == String.upcase(char)
 end
