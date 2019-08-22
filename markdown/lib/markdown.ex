0b5cc72cac59ce8848eef03d3ea4a9461f63c449
Matt Levy <milevy1@gmail.com>
2019-08-22 09:53:26 -0600

    Adds partial refactor of markdown Exercism

--- markdown/lib/markdown.ex
+++ markdown/lib/markdown.ex
@@ -11,24 +11,51 @@ defmodule Markdown do
     "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
-  def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+  def parse(input) do
+    # patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+    String.split(input, "\n")
+    |> Enum.map(fn line -> process(line) end)
+    |> Enum.join
+    |> patch
   end
 
-  defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
+  # defp process(t) do
+  #   if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
+  #     if String.starts_with?(t, "#") do
+  #       enclose_with_header_tag(parse_header_md_level(t))
+  #     else
+  #       parse_list_md_level(t)
+  #     end
+  #   else
+  #     enclose_with_paragraph_tag(String.split(t))
+  #   end
+  # end
+
+  defp process(input) when is_list(input) do
+
   end
 
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
+  defp process(input) when is_binary(input) do
+    [head | tail] = String.to_char(input)
+    process([head | tail])
+  end
+
+  defp process(<<first_char :: binary-size(1)>> <> _tail = line) when first_char == "#" do
+    parse_header_md_level(line)
+    |> enclose_with_header_tag
+  end
+
+  defp process(<<first_char :: binary-size(1)>> <> _tail = line) when first_char == "*" do
+    parse_list_md_level(line)
+  end
+
+  defp process(line) do
+    String.split(line)
+    |> enclose_with_paragraph_tag
+  end
+
+  defp parse_header_md_level(line) do
+    [h | t] = String.split(line)
     {to_string(String.length(h)), Enum.join(t, " ")}
   end
 
