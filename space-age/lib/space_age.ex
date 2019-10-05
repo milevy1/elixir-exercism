355a00cd8a8c70b54f7c1ae590f5dc7176c94589
Matt Levy <milevy1@gmail.com>
2019-10-05 16:55:52 -0500

    Adds space-age solution

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -15,5 +15,19 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    orbital_periods = %{
+      :mercury => 0.2408467,
+      :venus => 0.61519726,
+      :mars => 1.8808158,
+      :jupiter => 11.862615,
+      :saturn => 29.447498,
+      :uranus => 84.016846,
+      :neptune => 164.79132
+    }
+
+    case planet do
+      :earth -> seconds / 31_557_600
+      _ -> age_on(:earth, seconds) / orbital_periods[planet]
+    end
   end
 end
